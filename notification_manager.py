27997240276de9f4f85541274f089086eff7841f
Pearl Sai <pearlsai91@gmail.com>
2022-05-24 16:35:56 -0600

    Change the lowest price in Google sheet if current price is lower than price in the sheet

--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,18 @@
+import requests
+from twilio.rest import Client
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        self.account_sid = 'AC5291353c75429cc516071c422886464f'
+        self.auth_token = '4a52811e3dfb6ee416505f6ab2bc8594'
+
+    def send_message(self):
+        message_to_send = ""
+        client = Client(self.account_sid, self.auth_token)
+        message = client.messages \
+            .create(
+            body=message_to_send,
+            from_='+14707858749',
+            to='+18017590187'
+        )
+        print(message.status)
